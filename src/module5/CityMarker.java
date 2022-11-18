ff64aef5258c1150d446e3c5c8ebff2c8f59f1ff
Phillip Marques <phillip.silva@capgemini.com>
2022-11-18 19:39:36 -0300

    Module 5

--- src/module5/CityMarker.java
+++ src/module5/CityMarker.java
@@ -7,7 +7,8 @@ import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import processing.core.PConstants;
 import processing.core.PGraphics;
 
-/** Implements a visual marker for cities on an earthquake map
+/**
+ * Implements a visual marker for cities on an earthquake map
  * 
  * @author UC San Diego Intermediate Software Development MOOC team
  * @author Your name here
@@ -16,60 +17,64 @@ import processing.core.PGraphics;
 // TODO: Change SimplePointMarker to CommonMarker as the very first thing you do 
 // in module 5 (i.e. CityMarker extends CommonMarker).  It will cause an error.
 // That's what's expected.
-public class CityMarker extends SimplePointMarker {
-	
-	public static int TRI_SIZE = 5;  // The size of the triangle marker
-	
+public class CityMarker extends CommonMarker {
+
+	public static int TRI_SIZE = 5; // The size of the triangle marker
+
 	public CityMarker(Location location) {
 		super(location);
 	}
-	
-	
+
 	public CityMarker(Feature city) {
-		super(((PointFeature)city).getLocation(), city.getProperties());
+		super(((PointFeature) city).getLocation(), city.getProperties());
 		// Cities have properties: "name" (city name), "country" (country name)
 		// and "population" (population, in millions)
 	}
 
-	
 	/**
 	 * Implementation of method to draw marker on the map.
 	 */
 	public void draw(PGraphics pg, float x, float y) {
 		// Save previous drawing style
 		pg.pushStyle();
-		
+
 		// IMPLEMENT: drawing triangle for each city
 		pg.fill(150, 30, 30);
-		pg.triangle(x, y-TRI_SIZE, x-TRI_SIZE, y+TRI_SIZE, x+TRI_SIZE, y+TRI_SIZE);
-		
+		pg.triangle(x, y - TRI_SIZE, x - TRI_SIZE, y + TRI_SIZE, x + TRI_SIZE, y + TRI_SIZE);
+
 		// Restore previous drawing style
 		pg.popStyle();
 	}
-	
+
 	/** Show the title of the city if this marker is selected */
-	public void showTitle(PGraphics pg, float x, float y)
-	{
-		
-		// TODO: Implement this method
+	public void showTitle(PGraphics pg, float x, float y) {
+
+		pg.fill(255,255,255);
+		pg.rect(x-2,y-35,60,38);
+		pg.fill(0,0,0);
+		pg.text(this.getCity(), x, y-20);
+		pg.text(this.getCountry(), x, y-10);
+		pg.text(this.getPopulation(), x, y);
 	}
-	
-	
-	
-	/* Local getters for some city properties.  
+
+	/*
+	 * Local getters for some city properties.
 	 */
-	public String getCity()
-	{
+	public String getCity() {
 		return getStringProperty("name");
 	}
-	
-	public String getCountry()
-	{
+
+	public String getCountry() {
 		return getStringProperty("country");
 	}
-	
-	public float getPopulation()
-	{
+
+	public float getPopulation() {
 		return Float.parseFloat(getStringProperty("population"));
+	}
+
+	@Override
+	public void drawMarker(PGraphics pg, float x, float y) {
+		// TODO Auto-generated method stub
+		
 	}
 }
